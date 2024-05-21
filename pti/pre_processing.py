58f459586bbb42eecd86e0b71c427e3211e09370
Ioannis Tzortzis <105294556+itzortzis@users.noreply.github.com>
2024-05-21 11:09:21 +0300

    Update pre_processing.py

--- pti/pre_processing.py
+++ pti/pre_processing.py
@@ -35,7 +35,7 @@ class DataPreprocesing:
 
     def build_loaders(self):
         train_set      = loaders.Dataset(self.train_set_x, self.train_set_y)
-        params         = {'batch_size': 10, 'shuffle': False}
+        params         = {'batch_size': 10, 'shuffle': True}
         self.train_ldr = torch.utils.data.DataLoader(train_set, **params)
         valid_set      = loaders.Dataset(self.valid_set_x, self.valid_set_y)
         params         = {'batch_size': 10, 'shuffle': False}
